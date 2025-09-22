c962edd769987517f2ed58d2508dca9562de6104
Dario Rodríguez <72631983+dariblue@users.noreply.github.com>
2025-09-23 00:27:11 +0200

    Preparar despliegue

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0(*r!wb)ppt^qwoft=q5ffye(h=zxt_369r@a$wxt^l2&thx=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
