d445bf935aa6002fa9cb729e7cc8498b4a448e5e
Dario Rodríguez <72631983+dariblue@users.noreply.github.com>
2025-10-01 01:11:14 +0200

    postgrade o como se llame

--- project/settings.py
+++ project/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Django",
+        "USER": "d4r1",
+        "PASSWORD": "MSa04640",
+    "HOST": "bbdd-relecloud-d4r1.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
